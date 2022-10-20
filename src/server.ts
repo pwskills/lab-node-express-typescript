6a000c9fbb83462fbea88a2663eabe4c2744970f
Ankit Sharma <sankit945@gmail.com>
2022-10-20 16:38:24 +0530

    added comment to see output

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,16 @@ import express, { Request, Response } from 'express';
 
 const PORT = process.env.PUBLIC_PORT || 4000;
 
-//Initialize application
+// Initialize application
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
 });
 
-//Start server
+// Start server
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+
+// Go to your lab url - https://your-lab-url.ineuron.app:4000 to see the output
